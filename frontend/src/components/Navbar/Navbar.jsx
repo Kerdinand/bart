984aa3543767cdea71d65181e188dcd5fae1deea
Kerdinand <ferdinandjonas.kloth@t-online.de>
2021-12-05 21:53:21 +0100

    devided content between front and backend + readme added

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            This is a placeholder for the Navbar!
+        </div>
+     );
+}
+ 
+export default Navbar;
---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Navbar from './components/Navbar/Navbar';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Navbar></Navbar>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
